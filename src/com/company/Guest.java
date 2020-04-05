af72f2b0959971e0bb0669cce3299c372f53f717
Swati <60570301+swatissh@users.noreply.github.com>
2020-04-05 16:51:29 +0800

    Add files via upload

---
+++ src/com/company/Guest.java
@@ -0,0 +1,97 @@
+package HRPS;
+
+public class Guest {
+
+	private String name;
+	private int creditCardNum;
+	private String address;
+	private String country;
+	private String gender;
+	private String nationality;
+	private int phoneNumber;
+	private String drivingLicense;
+	private String passportNumber;
+	private String expiryDate;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public int getcreditCardNum() {
+		return creditCardNum;
+	}
+	
+	public void setcreditCardNum(int creditCardNum ) {
+		this.creditCardNum=creditCardNum;
+	}
+		
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address=address;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country=country;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		this.gender=gender;
+	}
+	
+	public String getNationality() {
+		return nationality;
+	}
+	
+	public void setNationality(String nationality) {
+		this.nationality=nationality;
+	}
+	
+	public int getphoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setphoneNumber(int phoneNumber) {
+		this.phoneNumber=phoneNumber;
+	}
+	
+	public String getdrivingLicense() {
+		return drivingLicense;
+	}
+	
+	public void setdrivingLicense(String drivingLicense) {
+		this.drivingLicense=drivingLicense;
+	}	
+	
+	public String getpassportNumber() {
+		return passportNumber;
+	}
+	
+	public void setpassportNumber(String passportNumber) {
+		this.passportNumber=passportNumber;
+	}
+		
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate=expiryDate;
+	}	
+	
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+	
+
+}
